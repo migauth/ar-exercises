0808f8b8557e4a5703453bfed71f0cc376b6e02c
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-03 16:57:27 -0700

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.second  
+
+puts @store1.inspect
+
+@store1.name = "Peterborough"
+
+puts @store1.inspect
