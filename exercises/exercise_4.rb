437affbf71cf5ad2c5516f7d10fbcc881eb5159b
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-04 10:37:39 -0700

    ex3,4,5 and add to store and employee models

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: 'Surrey', annual_revenue: 224000, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: nil)
+
+# Loop through each store and output its name
+@mens_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end
+
+# puts "----------"
+
+@womens_stores = Store.where(womens_apparel: true, mens_apparel: nil).where("annual_revenue < ?", 1000000)
+
+@womens_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Bobberson", hourly_rate: 70)
+@store1.employees.create(first_name: "Wendy", last_name: "Welches", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Will", last_name: "Butts", hourly_rate: 80)
+@store2.employees.create(first_name: "Pat", last_name: "Ricks", hourly_rate: 70)
+@store2.employees.create(first_name: "Amber", last_name: "Bamber", hourly_rate: 60)
